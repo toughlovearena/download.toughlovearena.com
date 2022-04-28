9e6824e2ed2714dcd9d0aaa77a8f527c3d3a9251
mpaulweeks <mpaulweeks@gmail.com>
2022-04-28 14:15:31 -0400

    autoHideMenuBar: true

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
